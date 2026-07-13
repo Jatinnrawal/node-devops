52ad370e0852336254cdaa365923a914ce48c66f
Jatin rawal <rawaljatin@gmail.com>
2026-07-13 12:21:32 +0530

    Test Webhook

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from Node DevOps demo!');
+    res.send('Hello from Node DevOps!');
 });
 
 app.get('/health', (req, res) => {
